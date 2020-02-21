810519968fc3ad35f0b9197f57605f20eecd9f77
eaajome <joshuabacuriomercado@gmail.com>
2020-02-21 11:24:32 +0100

    added boxes

--- pages/filter.js
+++ pages/filter.js
@@ -12,20 +12,40 @@ export default class FilterPage {
           <header class="topbar">
             <h2>Find det mest ideelle brætspil</h2>
           </header>
-
-          <div id="grid-boardgames" class="grid-container"></div>
+          <div id="playersWrapper">
+          <h2>Hvor mange spiller er I?</h2>
+          <div id="players">
+        
+          <button class="box"><a href="">1</a></button>
+          <button class="box"><a href="">2</a></button>
+          <button class="box"><a href="">3</a></button>
+          <button class="box"><a href="">4</a></button>
+          <button class="box"><a href="">5</a></button>
+          <button class="box"><a href="">6</a></button>
+          <button class="box"><a href="">7</a></button>
+          <button class="box"><a href="">8+</a></button>
+        </div>
+        </div>
+        <div id="playtime"></div>
+        <div id="genres"></div>
         </section>
       `;
   }
 
-  appendBoardgames(boardgames) {
-    for (let boardgame of boardgames) {
-      document.querySelector("#grid-boardgames").innerHTML += /*html*/ `
-        <article>
-          <img src="${boardgame.image_url}">
-          <h4>${boardgame.name}</h4>
-        </article>
-        `;
-    }
-  }
+
+
+  /*   
+            <div id="grid-boardgames" class="grid-container"></div>'
+            
+            
+            appendBoardgames(boardgames) {
+      for (let boardgame of boardgames) {
+        document.querySelector("#grid-boardgames").innerHTML += `
+          <article>
+            <img src="${boardgame.image_url}">
+            <h4>${boardgame.name}</h4>
+          </article>
+          `;
+      }
+    } */
 }
