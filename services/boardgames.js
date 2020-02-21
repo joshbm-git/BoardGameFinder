5ae06f9e7e3b02c7b418f1b63bfa5d1c9bc12837
eaajome <joshuabacuriomercado@gmail.com>
2020-02-21 10:37:15 +0100

    added boardgame service

---
+++ services/boardgames.js
@@ -0,0 +1,32 @@
+/*import loaderService from "./loader.js";
+ class BGService {
+  constructor() {
+    this.loaderService = loaderService;
+  } 
+  
+    async loadBoardgames() {
+    // insert api here
+    let response = await fetch("https://www.boardgameatlas.com/api/search?client_id=LtJ7MiiwkF");
+    let jsonData = await response.json();
+    console.log(jsonData);
+    this.loaderService.show(false);
+    return jsonData.games;
+  }*/
+
+class BoardgamesService {
+  constructor() {}
+
+  async loadBoardgames() {
+    // insert api here
+    let response = await fetch("https://www.boardgameatlas.com/api/search?client_id=LtJ7MiiwkF");
+    let jsonData = await response.json();
+    console.log(jsonData);
+    return jsonData.games;
+  }
+
+}
+
+/* client_id = LtJ7MiiwkF
+ */
+const boardgameService = new BoardgamesService();
+export default boardgameService;
